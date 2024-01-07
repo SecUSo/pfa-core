4b547226a106d97e06a78773dae90f97d360b56b
Patrick Schneider <urulu@student.kit.edu>
2024-01-07 19:22:01 +0100

    [fix] Improves styling of *MultiStageItem.

--- app/src/main/java/org/secuso/privacyfriendlycore/ui/stage/composables.kt
+++ app/src/main/java/org/secuso/privacyfriendlycore/ui/stage/composables.kt
@@ -3,32 +3,36 @@ package org.secuso.privacyfriendlycore.ui.stage
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+val multiStageItemModifier = { onClick: () -> Unit ->
+    Modifier
+        .fillMaxWidth()
+        .clickable { onClick() }
+        .padding(top = 16.dp, bottom = 16.dp, start = 32.dp, end = 32.dp) }
+
 @Composable
 fun SimpleMultiStageItem(name: String, onClick: () -> Unit) {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .clickable { onClick() }) {
+    Row(multiStageItemModifier(onClick)) {
         Text(text = name)
     }
 }
 
 @Composable
 fun IconMultiStageItem(icon: ImageVector, name: String, onClick: () -> Unit) {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .clickable { onClick() }) {
+    Row(multiStageItemModifier(onClick)) {
         Icon(imageVector = icon, contentDescription = null)
         Text(text = name)
     }
