9a51dcfaa96c686c1654a923f952be5c5c3aa0bb
Patrick Schneider <urulu@student.kit.edu>
2024-04-05 13:08:45 +0200

    [refactor] improves SettingThemeSelector

--- ui-compose/src/main/java/org/secuso/pfacore/ui/compose/settings/CommonSettings.kt
+++ ui-compose/src/main/java/org/secuso/pfacore/ui/compose/settings/CommonSettings.kt
@@ -1,26 +1,33 @@
 package org.secuso.pfacore.ui.compose.settings
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import org.secuso.pfacore.R
-object CommonSettings {
-    val themeSelectorKey = "settings_day_night_theme"
 
-    fun themeSelector(context: Context): Settings.Setting.() -> Unit {
+class SettingThemeSelector() {
+    enum class Mode {
+        SYSTEM,
+        LIGHT,
+        DARK;
+    }
+    companion object {
+        const val themeSelectorKey = "settings_day_night_theme"
+    }
+
+    fun build(): Settings.Setting.() -> Unit {
         return {
-            radio<String> {
+            radio<Mode> {
                 key = themeSelectorKey
-                default = "System"
+                default = Mode.SYSTEM
                 onUpdate = { when(it) {
-                    "System" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                    "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    "Dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    Mode.SYSTEM -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    Mode.LIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    Mode.DARK -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                 } }
                 title { resource(R.string.select_day_night_theme) }
                 summary {transform { state, value -> state.entries!!.find { it.value == value}!!.entry }}
                 entries {
                     entries(R.array.array_day_night_theme)
-                    values(listOf("System", "Light", "Dark"))
+                    values(SettingThemeSelector.Mode.entries.map { it })
                 }
             }
         }
