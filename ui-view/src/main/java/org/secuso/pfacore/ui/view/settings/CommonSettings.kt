37696842d59d915c401413e2f328476994cb845c
Patrick Schneider <urulu@student.kit.edu>
2024-05-04 19:59:56 +0200

    [feat] adds CommonSettings.kt for ui-view.

---
+++ ui-view/src/main/java/org/secuso/pfacore/ui/view/settings/CommonSettings.kt
@@ -0,0 +1,39 @@
+package org.secuso.pfacore.ui.view.settings
+
+import androidx.appcompat.app.AppCompatDelegate
+import org.secuso.pfacore.R
+
+class SettingThemeSelector {
+
+    enum class Mode {
+        SYSTEM,
+        LIGHT,
+        DARK;
+    }
+
+    companion object {
+        const val themeSelectorKey = "settings_day_night_theme"
+    }
+
+    fun build(): Settings.Setting.() -> RadioSetting.RadioData<String> {
+        return {
+            radio<String> {
+                key = themeSelectorKey
+                default = Mode.SYSTEM.toString()
+                onUpdate = {
+                    when (Mode.valueOf(it)) {
+                        Mode.SYSTEM -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                        Mode.LIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        Mode.DARK -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                }
+                title { resource(R.string.select_day_night_theme) }
+                summary { transform { state, value -> state.entries.find { it.value == value }!!.entry } }
+                entries {
+                    entries(R.array.array_day_night_theme)
+                    values(SettingThemeSelector.Mode.entries.map { it.toString() })
+                }
+            }
+        }
+    }
+}
