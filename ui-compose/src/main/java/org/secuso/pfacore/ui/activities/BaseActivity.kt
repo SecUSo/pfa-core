7d49cc051ebad8057469b513f98c1ae07aefb28a
Patrick Schneider <urulu@student.kit.edu>
2024-10-13 16:04:19 +0200

    [ui-compose] sets background to fill.

--- ui-compose/src/main/java/org/secuso/pfacore/ui/activities/BaseActivity.kt
+++ ui-compose/src/main/java/org/secuso/pfacore/ui/activities/BaseActivity.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
@@ -57,7 +59,7 @@ abstract class BaseActivity : AppCompatActivity() {
                         )
                     }
                 ) {
-                    Box(modifier = Modifier.padding(it)) {
+                    Surface(modifier = Modifier.padding(it).fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                         Content(application)
                     }
                 }
