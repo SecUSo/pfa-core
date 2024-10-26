4c28f7c0d2f1801c4584fa22b632f13aa91be38a
Patrick Schneider <urulu@student.kit.edu>
2024-10-26 12:27:33 +0200

    [theme] adjusts theming. again.

--- ui-compose/src/main/java/org/secuso/pfacore/ui/activities/DrawerActivity.kt
+++ ui-compose/src/main/java/org/secuso/pfacore/ui/activities/DrawerActivity.kt
@@ -160,7 +160,7 @@ fun DrawerMenuComp(drawer: DrawerMenu, selected: (DrawerElement) -> Boolean) {
                 label = { Text(item.name) },
                 icon = if (item.icon != null) { -> Icon(painter = painterResource(item.icon!!), contentDescription = item.name) } else null,
                 onClick = { if (context is Activity) item.onClick(context) },
-                colors = NavigationDrawerItemDefaults.colors(selectedContainerColor = MaterialTheme.colorScheme.primaryContainer),
+                colors = NavigationDrawerItemDefaults.colors(selectedTextColor = MaterialTheme.colorScheme.primary, selectedContainerColor = Color.Unspecified),
                 selected = selected(item)
             )
         }
